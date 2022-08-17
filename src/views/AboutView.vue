afd8f025e497137d1653d2d05efee5fe7183c0c6
simone <sdiricco.91@gmail.com>
2022-08-17 22:11:15 +0200

    initial porting

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Vu3 - electron Boilerplate</p>
+  </div>
+</template>
+
+
+<style scoped>
+
+.about{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
